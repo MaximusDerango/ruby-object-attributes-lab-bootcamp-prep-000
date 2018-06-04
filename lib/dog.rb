ae78f0df0b1ab3b1acc2c22473e53cd602fd8105
Maxim Dalton <maximcdalton@gmail.com>
2018-06-04 04:08:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,17 +2,19 @@ class Dog
   def initialize()
   end
   
-  def name=()
-    @name = 
+  def name=(name)
+    @name = name
   end
   
-  def breed=()
-    @breed = 
+  def breed=(breed)
+    @breed = breed
   end
   
   def name
+    puts @name
   end
   
   def breed
+    put @breed
   end
 end
